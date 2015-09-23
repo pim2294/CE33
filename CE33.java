da872a8d6aa7a2068699ca3bb5942d0cf31f1611
Pablo Meneses <theyellowshirt12@gmail.com>
2015-09-22 20:15:59 -0400

    initial

---
+++ CE33.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+  public class CE33 {
+
+				 	public static void main(String[] args) {
+							// int Password;
+									String username;
+											String password;
+													System.out.print("Enter Username and Password Seperated By a Space");
+															Scanner keyboard = new Scanner(System.in);
+																	// Password = keyboard.nextInt();
+																			username = keyboard.next();
+																					password = keyboard.next();
+																							if ((username.equals("pim2294")) && (password.equals("123456")))
+																									{
+																												System.out.println("Welcome pim2294!");
+																														}
+																																else {
+																																			System.out.println("username or password incorrect");
+																																					}
+																																						}
+																																						}
+
